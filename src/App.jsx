fd6b7b6e4f51a2aefaec3964998fa089ecb6e5c2
Akash <pandeyaakash7491@gmail.com>
2025-05-01 19:34:11 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
+
 import CompanyWebsite from './Components/Body'
 
 function App() {
   return (
     <div>
-      {/* <Navbar/>
-      <Hero/> */}
+     
       <CompanyWebsite/>
     </div>
   )
